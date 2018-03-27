ddc37dca166f40ab587f1c480759e67d16903d43
Mike Horwath <mike@mtau.com>
2018-03-27 14:26:51 -0500

    update egress only 80/443 for vpc-endpoint sg

--- sg.tf
+++ sg.tf
@@ -59,20 +59,10 @@ resource "aws_security_group" "private-endpoint-sg" {
   description = "private endpoint security group"
   vpc_id      = "${aws_vpc.the_vpc.id}"
 
-  ingress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
-
-    cidr_blocks = [
-      "0.0.0.0/0",
-    ]
-  }
-
-  ingress {
-    from_port = -1
-    to_port   = -1
-    protocol  = "icmp"
+  egress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
 
     cidr_blocks = [
       "0.0.0.0/0",
@@ -80,19 +70,9 @@ resource "aws_security_group" "private-endpoint-sg" {
   }
 
   egress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
-
-    cidr_blocks = [
-      "0.0.0.0/0",
-    ]
-  }
-
-  egress {
-    from_port = -1
-    to_port   = -1
-    protocol  = "icmp"
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
 
     cidr_blocks = [
       "0.0.0.0/0",
